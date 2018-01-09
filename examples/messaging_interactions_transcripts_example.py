ff1331f338fdd68fcdf0fc930fbc063c491148f6
Anthony Jones <ajones@liveperson.com>
2018-01-09 13:24:32 -0500

    Update MI Transcripts Example

--- examples/messaging_interactions_transcripts_example.py
+++ examples/messaging_interactions_transcripts_example.py
@@ -1,5 +1,5 @@
 """
-This example shows how to create a CSV MI transcripts flat file from the lp_api_wrapper library.
+This example shows how to create a Messaging Interactions transcripts CSV flat file from the lp_api_wrapper library.
 """
 
 from lp_api_wrapper import MessagingInteractions
@@ -24,34 +24,39 @@ body = {'start': {'from': start_from, 'to': start_to}}
 # Get data! (Only 1 offset) Great for testing...
 data = mi_conn.conversations(body=body)
 
-# For all data in date range use 'all_conversations' (May take a while...)
-# data = mi_conn.all_conversations(body=body)
-
 # Grab Conversational History Records from data.
 convo_records = data['conversationHistoryRecords']
 
+# For all data in date range use 'all_conversations'. (Handles all offsets)
+# Note: This method extracts 'conversationHistoryRecords' already.
+#       Look in messaging_interactions.py for more information.
+# convo_records = mi_conn.all_conversations(body=body)
+
 # Extract conversation level data
+# Look at Messaging Interactions API documentation to see the JSON data structure from conversations.
 transcripts = []
 for record in convo_records:
     for message in record['messageRecords']:
         try:
-            transcript = dict(account_id=record['info']['brandId'])
-            transcript['conversation_id'] = record['info']['conversationId']
-            transcript['device'] = record['info']['device']
-            transcript['text'] = message['messageData']['msg']['text']
-            transcript['message_id'] = message['messageId']
-            transcript['participant_id'] = message['participantId']
-            transcript['sent_by'] = message['sentBy']
-            transcript['seq'] = message['seq']
-            transcript['source'] = message['source']
-            transcript['time'] = message['time']
-            transcript['epoch_time'] = message['timeL']
+            transcript = {
+                'account_id': record['info']['brandId'],
+                'conversation_id': record['info']['conversationId'],
+                'device': record['info']['device'],
+                'text': message['messageData']['msg']['text'],
+                'message_id': message['messageId'],
+                'participant_id': message['participantId'],
+                'sent_by': message['sentBy'],
+                'seq': message['seq'],
+                'source': message['source'],
+                'time': message['time'],
+                'epoch_time': message['timeL']
+            }
             transcripts.append(transcript)
         except KeyError:
             # Skip over record if any record doesn't exist.
             continue
 
-# Only necessary if to move columns in a specific position
+# Only necessary if you want to position your columns in your flat file, column names
 cols = [
     'account_id',
     'conversation_id',
@@ -69,7 +74,10 @@ cols = [
 # Convert into Pandas DataFrame
 df = pd.DataFrame(transcripts)[cols]
 
+# File path with file name.
+file_path = './transcripts.csv'
+
 # Export into CSV with no index column
-df.to_csv(path_or_buf='transcripts.csv', index=False)
+df.to_csv(path_or_buf=file_path, index=False)
 
 # Now you have a Transcripts Flat File!
