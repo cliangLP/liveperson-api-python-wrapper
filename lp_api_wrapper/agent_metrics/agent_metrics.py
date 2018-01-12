cdbe0506c1ff032b4259a02840a63a5d903fe5d3
Anthony Jones <ajones@liveperson.com>
2018-01-12 16:14:02 -0500

    Update comments

--- lp_api_wrapper/agent_metrics/agent_metrics.py
+++ lp_api_wrapper/agent_metrics/agent_metrics.py
@@ -1,7 +1,7 @@
 """
 An unofficial native Python wrapper for the LivePerson Agent Metrics API.
 
-Reference:
+Documentation:
 https://developers.liveperson.com/data-messaging-agent-metrics-overview.html
 
 The LiveEngage Messaging Agent Metrics API provides information about the current state of messaging agents in the
@@ -33,8 +33,6 @@ __author__ = 'Anthony Jones'
 __email__ = 'ajones@liveperson.com'
 
 
-
-
 class AgentMetrics(LoginService):
     def __init__(self, auth: Union[UserLogin, OAuthLogin]) -> None:
         super().__init__(auth=auth)
@@ -43,11 +41,11 @@ class AgentMetrics(LoginService):
     def agent_status(self, status: Optional[List[str]] = None, agent_ids: Optional[List[str]] = None,
                      skill_ids: Optional[List[str]] = None, agent_group_ids: Optional[List[str]] = None) -> dict:
         """
+        Documentation:
+        https://developers.liveperson.com/data-messaging-interactions-methods-agent-status.html
+
         Returns the current state of logged in agents that are handling messaging conversations with all its
         related data, including status, number of open conversations, load, skills etc.
-
-        Reference:
-        https://developers.liveperson.com/data-messaging-interactions-methods-agent-status.html
 
         :param status: List of Agent’s statuses to be filtered
         :param agent_ids: List of agent ids - when provided, data will be returned for the specified agents who are in
@@ -82,11 +80,11 @@ class AgentMetrics(LoginService):
     def summary(self, status: Optional[List[str]] = None, agent_ids: Optional[List[str]] = None,
                 skill_ids: Optional[List[str]] = None, agent_group_ids: Optional[List[str]] = None) -> dict:
         """
+        Documentation:
+        https://developers.liveperson.com/data-messaging-interactions-methods-summary.html
+
         Returns a summary on the current state of the contact center. This includes number of agents in the different
         logged in statuses, weighted average of the agents’ load etc.
-
-        Reference:
-        https://developers.liveperson.com/data-messaging-interactions-methods-summary.html
 
         :param status: List of Agent's statuses to be filtered
         :param agent_ids: List of agent ids - when provided, data will be returned for the specified agents who are in
